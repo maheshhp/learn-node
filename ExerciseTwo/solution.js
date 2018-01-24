c2b29509d631b41dd1b726e041ddfcadc6fae7da
maheshhp <hpmahesh73@gmail.com>
2018-01-24 12:14:41 +0530

    Exercise Two: Passed tests

--- ExerciseTwo/solution.js
+++ ExerciseTwo/solution.js
@@ -1,2 +1,10 @@
-let addCLArgs = (...args) => {};
+let addCLArgs = (...args) => {
+  console.log(args);
+  if (args.length === 0) {
+    console.log(undefined);
+  } else {
+    let addedArgs = args.reduce((accumulate, element) => accumulate + element);
+    console.log(addedArgs);
+  }
+};
 module.exports = addCLArgs;
--- ExerciseTwo/solution.test.js
+++ ExerciseTwo/solution.test.js
@@ -6,6 +6,10 @@ global.console = {
 };
 
 describe('Tests for checking the console log by the function that adds everything in the arguments', () => {
+  test('Verify log value for no arguments', () => {
+    addCLArgs();
+    expect(global.console.log).toHaveBeenCalledWith(undefined);
+  });
   test('Verify log value for strings', () => {
     addCLArgs('Hello', 'World');
     expect(global.console.log).toHaveBeenCalledWith('HelloWorld');
@@ -13,8 +17,5 @@ describe('Tests for checking the console log by the function that adds everythin
   test('Verify log value for numbers', () => {
     addCLArgs(1, 2, 3, 4, 5);
     expect(global.console.log).toHaveBeenCalledWith(15);
-  });
-  test('Verify log value for no arguments', () => {
-    expect(global.console.log).toHaveBeenCalledWith(undefined);
   });
 });
