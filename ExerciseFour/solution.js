3f00e738b17dcc995a3e8f92460de7062afa0505
maheshhp <hpmahesh73@gmail.com>
2018-01-25 16:26:31 +0530

    Exercise Four: Added tests

---
+++ ExerciseFour/solution.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+let accessFileAsync = (arg) => {
+
+};
+
+// accessFile();
+module.exports = accessFileAsync;
--- ExerciseThree/solution.js
+++ ExerciseThree/solution.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
-let accessFile = () => {
-  let fileContentNewLines = fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1;
+let accessFile = (arg) => {
+  let fileContentNewLines = fs.readFileSync(arg, 'utf8').split('\n').length - 1;
   console.log(fileContentNewLines);
 };
 
-accessFile();
+// accessFile();
 module.exports = accessFile;
