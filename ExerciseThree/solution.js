b805f5d18a62cf3ed687a2dd69a01f1838285e95
maheshhp <hpmahesh73@gmail.com>
2018-01-25 11:38:45 +0530

    Exercise Three: Verified tests

--- ExerciseThree/solution.js
+++ ExerciseThree/solution.js
@@ -1,2 +1,7 @@
-let accessFile = () => {};
+const fs = require('fs');
+
+let accessFile = (arg) => {
+  let fileContentNewLines = fs.readFileSync(arg, 'utf8').split('\n').length;
+  console.log(fileContentNewLines);
+};
 module.exports = accessFile;
--- ExerciseThree/solution.test.js
+++ ExerciseThree/solution.test.js
@@ -7,14 +7,14 @@ global.console = {
 
 describe('Tests for checking the log of newline characters by the function that reads the contents of the file specified in the command line argument', () => {
   test('Verify error thrown for no file found', () => {
-    expect(accessFile('~/Documents/newTest.txt')).toThrowError(/file/);
+    expect(() => accessFile('/Documents/newTest.txt')).toThrowError('ENOENT: no such file or directory, open \'/Documents/newTest.txt\'');
   });
-  test('Verify log for file with no newline characters', () => {
-    accessFile('~/Documents/test.txt');
-    expect(global.console.log).toHaveBeenCalledWith(0);
+  test('Verify log for file with no content for newline characters', () => {
+    accessFile('/Users/maheshhp/Downloads/learnyounode/ExerciseThree/test.txt');
+    expect(global.console.log).toHaveBeenCalledWith(1);
   });
   test('Verify log for file with 5 newline characters', () => {
-    accessFile('~/Documents/testFive.txt');
-    expect(global.console.log).toHaveBeenCalledWith(5);
+    accessFile('/Users/maheshhp/Downloads/learnyounode/ExerciseThree/testFive.txt');
+    expect(global.console.log).toHaveBeenCalledWith(6);
   });
 });
