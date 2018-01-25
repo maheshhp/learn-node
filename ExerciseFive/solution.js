71cef6720e9ecc7cd2009a7b39ffc7087ce25347
maheshhp <hpmahesh73@gmail.com>
2018-01-25 19:41:24 +0530

    Exercise Five: Verified exercise

--- ExerciseFive/solution.js
+++ ExerciseFive/solution.js
@@ -1,7 +1,16 @@
 const fs = require('fs');
 
 let accessDirAsync = (dirPath, fileType, testCallback) => {
-
+  // let dirPath = process.argv[2];
+  // let fileType = process.argv[3];
+  fs.readdir(dirPath, 'utf8', (error, fileList) => {
+    if (error) {
+      return testCallback(error.code);
+    }
+    let retrievedFiles = fileList
+      .filter(filename => filename.includes(`.${fileType}`));
+    return testCallback(retrievedFiles);
+  });
 };
-
+// accessDirAsync();
 module.exports = accessDirAsync;
