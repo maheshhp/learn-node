97c531cfa20931cff9952ffd2f580dd81cd2ce36
maheshhp <hpmahesh73@gmail.com>
2018-01-25 20:52:46 +0530

    Exercise Six: Verified exercise

--- ExerciseSix/printToConsole.js
+++ ExerciseSix/printToConsole.js
@@ -1,6 +1,18 @@
 const getFilesAsync = require('./solution');
 
 let logToConsole = (dirPath, extension, testCallback) => {
-
+  // let dirPath = process.argv[2];
+  // let extension = process.argv[3];
+  getFilesAsync(dirPath, extension, (err, data) => {
+    if (err) {
+      console.log(err.code);
+      testCallback(err);
+    } else {
+      // data.forEach(element => console.log(element));
+      console.log(data.toString());
+      testCallback(null, data);
+    }
+  });
 };
+// logToConsole();
 module.exports = logToConsole;
--- ExerciseSix/solution.js
+++ ExerciseSix/solution.js
@@ -1,7 +1,14 @@
 const fs = require('fs');
 
 let accessDirAsync = (dirPath, fileType, printCallback) => {
-
+  fs.readdir(dirPath, 'utf8', (error, fileList) => {
+    if (error) {
+      return printCallback(error);
+    }
+    let retrievedFiles = fileList
+      .filter(filename => filename.includes(`.${fileType}`));
+    return printCallback(null, retrievedFiles);
+  });
 };
 
 module.exports = accessDirAsync;
