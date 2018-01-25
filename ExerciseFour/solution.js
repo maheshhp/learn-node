e92c0fbcc5541d385e0f3a88883106f413b170e8
maheshhp <hpmahesh73@gmail.com>
2018-01-25 17:02:51 +0530

    Exercise Four: Verified exercise

--- ExerciseFour/solution.js
+++ ExerciseFour/solution.js
@@ -1,8 +1,13 @@
 const fs = require('fs');
 
 let accessFileAsync = (arg) => {
-
+  fs.readFile(arg, 'utf8', (error, fileContent) => {
+    if (error) {
+      console.log(error.code);
+    } else {
+      console.log(fileContent.split('\n').length - 1);
+    }
+  });
 };
 
-// accessFile();
 module.exports = accessFileAsync;
--- ExerciseFour/solution.test.js
+++ ExerciseFour/solution.test.js
@@ -1,20 +1,15 @@
-const accessFile = require('./solution');
-
-global.console = {
-  warn: jest.fn(),
-  log: jest.fn(),
-};
+const accessFileAsync = require('./solution');
 
 describe('Tests for checking the log of newline characters by the function that reads the contents of the file using callback specified in the command line argument', () => {
-  test('Verify error thrown for no file found', () => {
-    expect(() => accessFile('/Documents/newTest.txt')).toThrowError('ENOENT: no such file or directory, open \'/Documents/newTest.txt\'');
+  test('Verify error code written to the console for no file found', () => {
+    accessFileAsync('/Users/maheshhp/Downloads/learnyounode/ExerciseThree/testMe.txt');
+    expect(accessFileAsync('/Users/maheshhp/Downloads/learnyounode/ExerciseThree/testMe.txt')).toMatch('ENOENT');
   });
   test('Verify log for file with no newline characters', () => {
-    accessFile('/Users/maheshhp/Downloads/learnyounode/ExerciseThree/test.txt');
-    expect(global.console.log).toHaveBeenCalledWith(0);
+    accessFileAsync('/Users/maheshhp/Downloads/learnyounode/ExerciseThree/test.txt');
+    expect(accessFileAsync('/Users/maheshhp/Downloads/learnyounode/ExerciseThree/test.txt')).toMatch('0');
   });
   test('Verify log for file with 5 newline characters', () => {
-    accessFile('/Users/maheshhp/Downloads/learnyounode/ExerciseThree/testFive.txt');
-    expect(global.console.log).toHaveBeenCalledWith(5);
+    expect(accessFileAsync('/Users/maheshhp/Downloads/learnyounode/ExerciseThree/testFive.txt')).toMatch('5');
   });
 });
