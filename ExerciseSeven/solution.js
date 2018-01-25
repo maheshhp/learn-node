f856c8f25885ba5a57efd0498755077bf3871878
maheshhp <hpmahesh73@gmail.com>
2018-01-25 21:26:28 +0530

    Exercise Seven: Verified exercise -- Issues with tests

--- ExerciseSeven/solution.js
+++ ExerciseSeven/solution.js
@@ -1,20 +1,25 @@
 const http = require('http');
 
-let accessHTTPAsync = (getURL, testCallback) => {
+let accessHTTPAsync = () => {
+  let getURL = process.argv[2];
   http.get(getURL, (response) => {
+    response.setEncoding('UTF8');
+    let retData = [];
     response.on('data', (data) => {
+      retData.push(data);
       // console.log(data);
-      testCallback('DATA');
-    });
-    response.on('error', (error) => {
-      // console.log(error);
-      testCallback('ERROR');
+      // testCallback('DATA');
     });
     response.on('end', (end) => {
       // console.log(end);
-      testCallback('END');
+      // testCallback('END');
+      retData.forEach(data => console.log(data));
+    });
+    response.on('error', (error) => {
+      console.log(error);
+      // testCallback('ERROR');
     });
   });
 };
-
+accessHTTPAsync();
 module.exports = accessHTTPAsync;
