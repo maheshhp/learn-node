db94ee4e4cafb62825c45c3a4766897801407240
maheshhp <hpmahesh73@gmail.com>
2018-01-28 10:13:53 +0530

    Exercise Eleven: Verified exercise

--- ExerciseEleven/solution.js
+++ ExerciseEleven/solution.js
@@ -1,4 +1,24 @@
-let setupHTTPServer = (port, filePath) => {
+const http = require('http');
+const fs = require('fs');
+const httpShutdown = require('http-shutdown'); // Required to gracefully shutdown the server after the first response
 
+let stopHTTPServer = (serverObject) => {
+  serverObject.shutdown(() => {
+    console.log('Everything is cleanly shutdown.');
+  });
 };
+
+let setupHTTPServer = (port, filePath) => {
+  let server = http.createServer((request, response) => {
+    let fileReadStream = fs.createReadStream(filePath);
+    fileReadStream.pipe(response);
+    fileReadStream.on('close', () => {
+      response.end();
+      stopHTTPServer(server);
+    });
+  });
+  server = (httpShutdown)(server);
+  server.listen(port);
+};
+// setupHTTPServer(8000, '');
 module.exports = setupHTTPServer;
--- ExerciseEleven/solution.test.js
+++ ExerciseEleven/solution.test.js
@@ -5,10 +5,10 @@ const fs = require('fs');
 describe('Tests for checking the HTML data received from the HTTP server', () => {
   test('Verify the HTML data received from the HTTP server on request', (done) => {
     let retData = '';
-    let filePath = '/Users/maheshhp/Downloads/learnyounode/ExerciseThree/test.txt';
+    let filePath = '/Users/maheshhp/Downloads/learnyounode/ExerciseEleven/index.html';
     let fileData = fs.readFileSync(filePath, 'UTF8');
     setupHTTPServer(8000, filePath);
-    http.get('127.0.0.1:8000', (response) => {
+    http.get('http://127.0.0.1:8000', (response) => {
       response.setEncoding('UTF8');
       response.on('data', (data) => {
         retData += data.toString();
