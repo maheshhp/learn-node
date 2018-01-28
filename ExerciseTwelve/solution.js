91af4f32d5e86016996dae98286cff3a3e9313fd
maheshhp <hpmahesh73@gmail.com>
2018-01-28 18:20:13 +0530

    Exercise Twelve: Verified exercise, Invalid input test failing - blocker

--- ExerciseTwelve/solution.js
+++ ExerciseTwelve/solution.js
@@ -2,28 +2,31 @@ const http = require('http');
 const through2Map = require('through2-map');
 const httpShutdown = require('http-shutdown'); // Required to gracefully shutdown the server after the first response
 
-let toUpperMapper = through2Map((reqBody) => {});
+let toUpperMapper = through2Map(reqBody => reqBody.toString().toUpperCase());
+let serverObject = [];
 
 // Server shut down helper function
 // Arguments: Server object
 // Returns: null
 // Output: Prints status to console
-let stopHTTPServer = (serverObject) => {
-  serverObject.shutdown(() => {
+let stopHTTPServer = (stopPort) => {
+  serverObject[stopPort].shutdown(() => {
     console.log('Everything is cleanly shutdown.');
   });
 };
 
 let startNodeServer = (port) => {
   let server = http.createServer((request, response) => {
-    if (request.method == 'POST') {
+    if (request.method === 'POST') {
       // Process body and pipe it to response
+      console.log(serverObject[port]);
+      request.pipe(toUpperMapper).pipe(response);
     }
-    stopHTTPServer(server);
   });
   server = (httpShutdown)(server);
+  serverObject[port] = server;
   server.listen(port);
 };
 
-
-module.exports = startNodeServer;
+// startNodeServer(process.argv[2]);
+module.exports = { startNodeServer, stopHTTPServer };
--- ExerciseTwelve/solution.test.js
+++ ExerciseTwelve/solution.test.js
@@ -1,4 +1,5 @@
-const getPOSTArgs = require('./solution');
+const getPOSTArgs = require('./solution').startNodeServer;
+const stopResponseServer = require('./solution').stopHTTPServer;
 const request = require('request');
 
 let requestHeader = {
@@ -10,21 +11,42 @@ let requestOptions = {
   url: 'http://127.0.0.1:8000',
   method: 'POST',
   headers: requestHeader,
-  form: { body: 'Hello World' },
+  form: 'HelloWorld',
 };
 
-describe('Tests for valid request body string', () => {
+let invalidRequestOptions = {
+  url: 'http://127.0.0.1:9000',
+  method: 'POST',
+  headers: requestHeader,
+  form: '1234',
+};
+
+describe('Tests for valid and invalid string in request body', () => {
   test('Verify the HTML data received from the HTTP server on request', (done) => {
     let retData = '';
     getPOSTArgs(8000);
     request(requestOptions, (error, response, body) => {
       if (!error && response.statusCode === 200) {
         retData += body;
-        console.log(body);
-        expect(retData).toMatch('');
+        expect(retData).toMatch('HELLOWORLD');
       } else {
         expect(retData).toMatch('ERROR');
       }
+      stopResponseServer(8000);
+      done();
+    });
+  });
+  test('Verify the HTML data received from the HTTP server on request', (done) => {
+    let retData = '';
+    getPOSTArgs(9000);
+    request(invalidRequestOptions, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        retData += body;
+        expect(retData).toMatch('1234');
+      } else {
+        expect(retData).toMatch('ERROR');
+      }
+      stopResponseServer(9000);
       done();
     });
   });
