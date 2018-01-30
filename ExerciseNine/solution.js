c24ce8c3801905c74d902e696fa0dd0e93f94ed3
maheshhp <hpmahesh73@gmail.com>
2018-01-30 08:02:10 +0530

    Exercise Nine: Verified exercise

--- ExerciseNine/solution.js
+++ ExerciseNine/solution.js
@@ -2,12 +2,26 @@ const bl = require('bl');
 const http = require('http');
 
 const printToConsole = (resultArr, call) => {
-
+  resultArr.forEach(x => call(x));
 };
 
 const collectResponse = (urlArray, callback) => {
-
+  let result = [];
+  let count = 0;
+  urlArray.forEach(url =>
+    http.get(url, (response) => {
+      response.pipe(bl((err, data) => {
+        if (err) {
+          callback(err.code);
+        }
+        result[count] = data.toString();
+        count += 1;
+        if (count === 3) {
+          printToConsole(result, callback);
+        }
+      }));
+    }));
 };
 
-module.exports = collectResponse;
-// collectResponse(process.argv.slice(2), console.log);
+// module.exports = collectResponse;
+collectResponse(process.argv.slice(2), console.log);
